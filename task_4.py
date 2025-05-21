22824510eb482ca4a02e07a2fccca3bd06ed0639
liuzong111 <3299627668@qq.com>
2025-05-21 20:45:41 +0800

    Add files via upload

---
+++ task_4.py
@@ -0,0 +1,24 @@
+def find_max_reverse_complement(mRNA):
+    # Define the complement rules (for mRNA)
+    complement = {'A':'U', 'U':'A', 'C':'G', 'G':'C'}
+    max_len = 0
+    n = len(mRNA)
+    
+    # Generate all possible substring pairs
+    for i in range(n):
+        for j in range(i+1, n+1):
+            # Reverse complement of the current substring
+            rc = ''.join([complement[base] for base in mRNA[i:j][::-1]])
+            curr_len = j - i
+            
+            # Search for a match of the same length in the subsequent region
+            for x in range(j, n - curr_len + 1):
+                y = x + curr_len
+                if mRNA[x:y] == rc:
+                    max_len = max(max_len, curr_len)
+    
+    return max_len
+
+# Example test
+test_seq = "AUCGCGAUAUCG"  # Contains two segments: AUCG and CGAU
+print(find_max_reverse_complement(test_seq.upper()))  # Output: 4
